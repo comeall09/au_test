fd9dc854712fba087e56fc6d723f39ef7ca00f93
ComeAll <kamolkhamidov200002@gmail.com>
2022-07-17 14:57:34 +0300

    modal window done

--- src/widgets/ui/StatusBar/StatusBar.tsx
+++ src/widgets/ui/StatusBar/StatusBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import s from './styles/statusBar.module.scss'
 import { StatusHeader } from './StatusHeader/StatusHeader'
 import { StatusModal } from './StatusModal/StatusModal'
@@ -7,15 +7,24 @@ export const StatusBar = () => {
   
   const [isActivated, setIsActivated] = useState(false)
 
+  const changeStatus = () => {
+    setIsActivated(true)
+  }
+
   const [status, setStatus] = useState('Прежде чем действовать, надо понять') // default status value
 
   return (
     <div className={s.statusBar}>
       <StatusHeader />
       <div className={s.statusWrap}>
-        <button onClick={() => setIsActivated(true)} className={s.btn}>Сменить статус</button>
+        <button onClick={changeStatus} className={s.btn}>Сменить статус</button>
         <div className={s.status}>{status}</div>
-        <StatusModal isActivated={isActivated} status={status} setStatus={setStatus}/>
+        <StatusModal 
+          isActivated={isActivated}
+          setIsActivated={setIsActivated}
+          status={status}
+          setStatus={setStatus}
+        />
       </div>
     </div>
   )
--- src/widgets/ui/StatusBar/StatusModal/StatusModal.tsx
+++ src/widgets/ui/StatusBar/StatusModal/StatusModal.tsx
@@ -3,32 +3,58 @@ import s from '../styles/statusBar.module.scss'
 
 interface modalProps {
   isActivated: boolean,
+  setIsActivated: (value: boolean) => void
   status: string,
   setStatus: (value: string) => void
 }
 
-export const StatusModal = ({isActivated, status, setStatus}: modalProps) => {
+export const StatusModal = ({isActivated, setIsActivated, status, setStatus}: modalProps) => {
 
+  // focus input when modal is opened
   const inputEl = useRef<HTMLInputElement>(null)
-  // useEffect(() => {
-  //   inputEl.current?.focus()
-  // }, [isActive])
-  const statusHandler = (value: string) => {
-    setStatus(value)
-  }
+  useEffect(() => {
+    inputEl.current?.focus()
+  }, [isActivated])
   
+  const [isMaxLengthed, setIsMaxLengthed] = useState(false)
+
+  const [newStatus, setNewStatus] = useState(status)
+  const statusHandler = (value: string) => {
+    setNewStatus(value)
+    if(value.length > 99) {
+      setIsMaxLengthed(true)
+    } else {
+      setIsMaxLengthed(false)
+    }
+  }
+
+  const changeStatus = (isChange: boolean) => {
+    if(isChange){
+      setStatus(newStatus)
+    } else {
+      setNewStatus(status)
+    }
+    setIsActivated(false)
+  }
+
   return (
-    <div className={s.statusModalWrap}>
-      <div className={s.statusModal}>
+    // <div className={`${s.statusModalWrap} ${isActivated && s.activeModal}`}>
+      <div className={`${s.statusModal} ${isActivated && s.activeModal}`}>
         <input
           className={s.statusInput}
-          value={status}
+          value={newStatus}
           ref={inputEl}
           onChange={e => statusHandler(e.target.value)}
           maxLength={100}
         />
-        <button className={`${s.btn} ${s.saveStatusBtn} `}>Сохранить статус</button>
+        <div className={s.modalBtns}>
+          <div className={`${s.statusMaxLength} ${isMaxLengthed && s.maxLengthed}`}>Не более 100 символов</div>
+          <div>
+            <button onClick={() => changeStatus(true)} className={s.confirmBtn}>Сохранить статус</button>
+            <button onClick={() => changeStatus(false)} className={s.cancelBtn}>Отменить</button>
+          </div>
+        </div>
       </div>
-    </div>
+    // </div>
   )
 }
